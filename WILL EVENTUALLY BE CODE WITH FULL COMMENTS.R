6aa9ada9e8c72ed41edf4bb4904a2cd7ebc46297
tallulahcoyte <tallulahcoyte@icloud.com>
2020-11-26 01:35:26 +0000

    Committing 2 new files and 4 edited files.

---
+++ WILL EVENTUALLY BE CODE WITH FULL COMMENTS.R
@@ -0,0 +1,5 @@
+# Going to start analysing the North West Atlantic datasets from 1973-80 and 1981-90.
+# Firstly I'm going to read the datasets into R.
+#
+NWA73_80 <- read.csv("NWAtlantic_Data/NWATLANTIC73_80.csv")
+NWA81_90 <- read.csv("NWAtlantic_Data/NWATLANTIC81_90.csv")
